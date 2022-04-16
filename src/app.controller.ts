cc157170e5653aa234c14184cfdebbfa1767e94f
NicholasWM <nicholaswesleymazzei@gmail.com>
2022-04-16 09:35:53 -0300

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { CreateUserRequest } from './create-user.dto';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('analytics')
+  getAnalytics() {
+    return this.appService.getAnalytics();
+  }
+
+  @Post()
+  createUser(@Body() createUserRequest: CreateUserRequest) {
+    this.appService.createUser(createUserRequest);
+  }
+}
